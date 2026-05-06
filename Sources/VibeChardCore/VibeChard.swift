af27194adcadd73c7ad0464218b391c401a0f81e
Maples7 <maples7@163.com>
2026-05-06 18:58:35 +0800

    chore: revert version bump (defer 0.1.2 release)
    Keep vch open on master but hold the version constant at 0.1.1 until
    we're ready to actually cut v0.1.2. Avoids advertising a phantom
    version on HEAD that has no matching git tag or Homebrew release.

--- Sources/VibeChardCore/VibeChard.swift
+++ Sources/VibeChardCore/VibeChard.swift
@@ -11,7 +11,7 @@ public enum VibeChard {
     /// Semantic version of the `vch` binary.
     /// Bumped manually in `git tag v*` releases. The release workflow
     /// fails fast if this constant doesn't match the pushed tag.
-    public static let version = "0.1.2"
+    public static let version = "0.1.1"
 
     /// Marketing tagline. Surfaces in `vch help` and `vch version --json`.
     public static let tagline =
